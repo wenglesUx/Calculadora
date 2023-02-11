47c7ca839679f11fe100cd52943ebfdf600258c7
wenglesUx <85462594+wenglesUx@users.noreply.github.com>
2023-02-11 11:52:16 -0300

    atualização de estilos

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
+let equal = document.querySelector('#igu');
+let back = document.querySelector('#back');
+let c = document.querySelector('#c')
+
+equal.addEventListener('click',calculation)
+back.addEventListener('click',erase)
+c.addEventListener('click',clear)
 
 
-function insert(x){
+
+function insert(num){
  const number = document.querySelector('#screen').innerHTML ;
- document.querySelector('#screen').innerHTML = number + x;
+ document.querySelector('#screen').innerHTML = number + num;
 }
 
- let c = document.querySelector('#c')
- c.addEventListener('click',clear)
- function clear(){
+
+function clear(){
     document.querySelector('#screen').innerHTML = ""
- }
+}
 
- let back = document.querySelector('#back');
-back.addEventListener('click',()=>{
+function erase(){
+        var x = document.querySelector('#screen').innerHTML;
+        document.querySelector('#screen').innerHTML = x.substring(0,x.length -1) 
+}
 
-        var resultado = document.querySelector('#screen').innerHTML;
-        document.querySelector('#screen').innerHTML = resultado.substring(0,resultado.length -1)
-})
-let equal  = document.querySelector('#igu')
-equal.addEventListener('click',()=>{
-    var resultado =  document.querySelector('#screen').innerHTML;
-    if(resultado){
-        document.querySelector('#screen').innerHTML = eval(resultado)
+function calculation(){
+    var x =  document.querySelector('#screen').innerHTML;
+    if(x){
+        document.querySelector('#screen').innerHTML = eval(x)
     } else{
-        document.querySelector('#screen').innerHTML = "nada para calcular"
+        document.querySelector('#screen').innerHTML = "empty"
     }
-})
+}
